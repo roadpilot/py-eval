8779af62be7b324746b3927c47aa95c527ce15f5
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-12-08 22:10:09 +0000

    solution

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # parse list of commands from input and execute the commands
+
+if __name__ == '__main__':
+    N = int(raw_input())
+    l = []
+    for _ in range(N):
+        s = raw_input().split()
+        cmd = s[0]
+        args = s[1:]
+        if cmd !="print":
+            cmd += "("+ ",".join(args) +")"
+            eval("l."+cmd)
+        else:
+            print l
